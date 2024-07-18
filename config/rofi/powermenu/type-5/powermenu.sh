abeccf8e678a54fdfe28a705cda4fd30cf6376b9
Jann Louie Almirante <jannlouiealmirante@gmail.com>
2024-07-18 22:35:28 +0800

    Fix powermenu

--- config/rofi/powermenu/type-5/powermenu.sh
+++ config/rofi/powermenu/type-5/powermenu.sh
@@ -74,7 +74,9 @@ run_cmd() {
 			amixer set Master mute
 			systemctl suspend
 		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+			if [[ "$DESKTOP_SESSION" == 'hyprland' ]]; then
+              hyprctl dispatch exit 0
+			elif [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
 				openbox --exit
 			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
 				bspc quit
